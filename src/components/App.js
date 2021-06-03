070a27bf78cf091a2a378557cb62ecdb44178e0c
nikoguerrero <nclguerrero@gmail.com>
2021-06-03 11:55:44 -0400

    cambios para probar comandos de colaboración

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+import pokemon from '../data/pokemon/pokemon.js';
+ console.log(pokemon);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
+
+const play = document.getElementById("play");
+play.addEventListener("click", clickPlay);
+
+function clickPlay() {
+    
+}
