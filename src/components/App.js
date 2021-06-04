e32f54347791858ada00a2b906c4d5f52ed58571
nikoguerrero <nclguerrero@gmail.com>
2021-06-04 13:48:39 -0400

    Esqueleto de html desde app.js. Se imprime la data de pokemon.js en pantalla

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@
 // JavasSript que contenga el `export` correspondiente...
 //
 import pokemon from '../data/pokemon/pokemon.js';
- console.log(pokemon);
+ console.log(pokemon.items);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -13,14 +13,45 @@ import pokemon from '../data/pokemon/pokemon.js';
 //   .then(console.log)
 //   .catch(console.error);
 //
+const data = pokemon.items;
+let gameCards = [];
 
 const App = () => {
-  const el = document.createElement('div');
+  
+  //manipulacion dinamica de DOM
+  const content = document.createElement('div');
+  const header = document.createElement('header');
+  content.appendChild(header);
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+  const title = document.createElement('h1');
+  title.innerText = 'Memory match';
+  header.appendChild(title);
 
-  return el;
+  const section = document.createElement('section');
+  content.appendChild(section);
+
+  let grid = document.createElement('div');
+  grid.className = 'grid';
+  section.appendChild(grid);
+  
+  
+  for(let i = 0; i < data.length; i++) {
+    let card = document.createElement('img');
+    card.setAttribute('src', data[i].image);
+    card.setAttribute('data-id', i);
+    // card.addEventListener('click', flipcard)
+    grid.appendChild(card)
+  }
+
+  //girar tarjeta
+  function flipCard() {
+    let gameCards = gameCards 
+  }
+
+
+
+
+  return content;
 };
 
 export default App;
--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
 
-const play = document.getElementById("play");
-play.addEventListener("click", clickPlay);
+// const play = document.getElementById("play");
+// play.addEventListener("click", clickPlay);
 
-function clickPlay() {
+// function clickPlay() {
     
-}
+// }
