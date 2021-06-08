19d602f99ac281ca2d19de83540fc6c9e4ffd75a
nikoguerrero <nclguerrero@gmail.com>
2021-06-08 11:30:44 -0400

    se añade data howl para cartas a trabajar

---
+++ src/data/howl/howl.js
@@ -0,0 +1,16 @@
+export default {
+    name: 'Howl',
+    items: [
+      { id: 'calucipher', image: 'src/data/howl/images/calcipher.png', bgColor: '#ffffff' },
+      { id: 'calcipher2', image: '/images/calcipher2.png', bgColor: '#ffffff' },
+      { id: 'castle', image: '/images/castle.png', bgColor: '#ffffff' },
+      { id: 'dog', image: '/images/dog.png', bgColor: '#ffffff' },
+      { id: 'howl', image: '/images/howl.png', bgColor: '#ffffff' },
+      { id: 'markl', image: '/images/markl.png', bgColor: '#ffffff' },
+      { id: 'sophie', image: '/images/sophie.png', bgColor: '#ffffff' },
+      { id: 'thewitch', image: '/images/thewitch.png', bgColor: '#ffffff' },
+      { id: 'turniphead', image: '/images/turniphead.png', bgColor: '#ffffff' },
+    ],
+  };
+
+  
