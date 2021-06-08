c7b3ee51c914d8bce3407a179ec5748f3c0dd97c
Mayrenejose <maye199226@gmail.com>
2021-06-08 08:08:10 -0400

    se agregaron imagenes y modifico boton

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import App from './components/App.js';
 
 const play = () => {
     btn.style.display = 'none';
+    castle.style.display = 'none';
     document.getElementById('root').appendChild(App());
 }
 
 const btn = document.createElement('button');
-btn.innerHTML = 'Jugar';
+btn.className = 'buttonPlay';
+btn.innerHTML = 'Jugar!';
 btn.addEventListener('click', play);
 document.body.appendChild(btn);
+
+const castle = document.createElement('img');
+castle.className = 'castleOne';
+castle.src = '/images/castle.png';
+document.body.appendChild(castle);
+
