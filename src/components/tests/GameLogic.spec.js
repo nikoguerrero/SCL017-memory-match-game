386ca33cfc3ad90aadd6fb8a85cbbae1e27be29f
nikoguerrero <nclguerrero@gmail.com>
2021-06-30 14:00:33 -0400

    cambio en código de test GameLogic.js. Se elimina for loop

--- src/components/tests/GameLogic.spec.js
+++ src/components/tests/GameLogic.spec.js
@@ -4,13 +4,6 @@ describe('GameLogic', () => {
     it('should shuffle an array', () => {
       const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
       const shuffledData = shuffle(data);
-      let areEqual = true;
-      for (let i = 0; i < data.length; i++) {
-        if (data[i] != shuffledData[i]) {
-          areEqual = false;
-          break;
-        }
-      }
-      expect(areEqual).toBe(false);
+      expect(shuffledData).not.toStrictEqual(data);
     });
 });
